c589c7a5ae49a61c1cf728ce911efb03e8526aa7
deber116 <deber116@gmail.com>
2020-03-18 21:11:50 -0500

    met prompt

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,42 @@
 class EmployeesController < ApplicationController
+    def index 
+        @employees = Employee.all
+    end
+
+    def new 
+        @emp = Employee.new
+    end
+
+    def show
+        @emp = Employee.find(params[:id])
+    end
+
+    def edit
+        @emp = Employee.find(params[:id])
+    end
+
+    def update
+        @emp = Employee.find(params[:id])
+        @emp.assign_attributes(strong_params)
+        if @emp.valid?
+            @emp.save
+            redirect_to employee_path(@emp)
+        else
+            render :edit
+        end
+    end
+
+    def create
+        @emp = Employee.new(strong_params)
+        if @emp.valid?
+            @emp.save
+            redirect_to employee_path(@emp)
+        else
+            render :new
+        end
+    end
+
+    def strong_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+    end
 end
