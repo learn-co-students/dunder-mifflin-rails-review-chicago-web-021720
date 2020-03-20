f6c31ea149b11cf9450bf65cc32e3eec70a446a3
cooljasonmelton <cooljasonmelton>
2020-03-20 00:33:58 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -3,12 +3,20 @@ class DogsController < ApplicationController
     def index
         @dogs = Dog.all
         @popular_dogs = Dog.popular_dog_array
-
+        @button = true
     end 
 
     def show
         @dog = Dog.find(params[:id])
     end 
 
+    def update
+        @button = false
+        @dogs = Dog.all
+        @popular_dogs = Dog.popular_dog_array
+        render :index 
+    end 
+
+  
 
 end
