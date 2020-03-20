44630a9c944a6937ef6e455e4d69a6e47eedb7b2
cooljasonmelton <cooljasonmelton>
2020-03-19 23:46:05 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,14 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+        @popular_dogs = Dog.popular_dog_array
+
+    end 
+
+    def show
+        @dog = Dog.find(params[:id])
+    end 
+
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,53 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end 
+
+    def show
+        find_employee
+    end 
+
+    def new
+        @employee = Employee.new
+    end 
+
+    def create
+        @employee = Employee.new(employee_params)
+        if @employee.valid?
+            @employee.save
+            redirect_to employee_path(@employee)
+        else
+            @errors = @employee.errors.full_messages
+            render :new
+        end 
+    end
+
+    def edit
+        find_employee
+    end 
+
+    def update
+        find_employee.assign_attributes(employee_params)
+        if find_employee.save
+            redirect_to employee_path(@employee)
+        else
+            @errors = @employee.errors.full_messsages
+            render :edit 
+        end   
+    end 
+
+ 
+    
+    private
+    def find_employee
+        @employee = Employee.find(params[:id])
+    end 
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end 
+
+
 end
+ 
